64bc6395f6a19d7e55a82740b39788576c95c05c
Mirza Ibrahim <ibrahimafroz77@gmail.com>
2025-06-24 17:42:49 +0300

    FIX BASE PATH

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", // <-- Add this line
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000", // Adjust to your backend port
+      "/api": "http://localhost:5000",
     },
   },
   build: {
